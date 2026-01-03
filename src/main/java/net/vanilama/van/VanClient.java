6ba09a1125cae8d20c88992f4ea87d31a3a73f28
Pixel Art Editor User <pixel.art.editor@example.com>
2026-01-03 17:55:44 +0100

    Initial commit

---
+++ src/main/java/net/vanilama/van/VanClient.java
@@ -0,0 +1,10 @@
+package net.vanilama.van;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/vanilama/van/VanDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vanilama.van;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

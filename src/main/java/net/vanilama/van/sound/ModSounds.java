ef6df602a4b36ec895d727ee028b83ad934811d1
Pixel Art Editor User <pixel.art.editor@example.com>
2026-01-06 11:08:53 +0100

    up to ep 24 with problem: magic block sounds not playing

--- src/main/java/net/vanilama/van/sound/ModSounds.java
+++ src/main/java/net/vanilama/van/sound/ModSounds.java
@@ -1,7 +1,10 @@
 package net.vanilama.van.sound;
 
+import net.minecraft.block.jukebox.JukeboxSong;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
